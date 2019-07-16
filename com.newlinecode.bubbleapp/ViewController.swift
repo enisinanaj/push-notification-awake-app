3a18c61cca97889a2e825024b21feb5906365252
Eni Sinanaj <enisinanaj@outlook.com>
2019-07-16 19:17:52 +0200

    Initial Commit

---
+++ com.newlinecode.bubbleapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.newlinecode.bubbleapp
+//
+//  Created by Eni Sinanaj on 16/07/2019.
+//  Copyright © 2019 Eni Sinanaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
